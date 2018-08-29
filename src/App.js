c2a625ade1b9d9d13fff743237fa6e6e0911484b
Ebrma <ebrimaconta@gmail.com>
2018-08-29 21:19:09 +0100

    iterate through

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import BigCalendar from 'react-big-calendar'
 import moment from 'moment';
-import data from './data/data.js';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
  
 BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
@@ -15,7 +14,27 @@ class App extends Component {
   }
 componentDidMount() {
     // fetch data and update state
-    this.setState({ data: data}) 
+    fetch("https://assessments.bzzhr.net/calendar/?format=json")
+   .then(function(response) {
+    return response.json();
+  })
+  .then(data => {
+    // you can access your data 
+     
+    let events = data.map((event) =>{
+      const start = event.start.substr(0, 10).split('-') ;
+      const end = event.end.substr(0, 10).split('-');
+        let obj = {
+          title: event.label,
+          start:  new Date(Number(start[0]), Number(start[1]), Number(start[2])),
+          end:  new Date(Number(end[0]), Number(end[1]), Number(end[2])),
+           
+        }
+          return obj;
+    });
+      this.setState({ data: events});
+
+})
  }
  eventStyleGetter(event, start, end, isSelected) {
     
@@ -28,31 +47,18 @@ componentDidMount() {
         border: '0px',
         display: 'block'
     };
-     
+     console.log(event.title);
     return {
         style: style
     };
 }
 render(){
-   console.log(this.state.data);
+ 
              
     return (
             <BigCalendar
                 culture='en'
-                  events={ [
-                    {
-                      'title': 'All Day Event very long title',
-                      'bgColor': 'FF69B4',
-                      'allDay': true,
-                      'start': new Date(2018, 7, 8),
-                      'end': new Date(2018,9 , 11)
-                    },{
-                      'title': 'All Day Event very long title',
-                      'bgColor': 'FF69B4',
-                      'allDay': true,
-                      'start': new Date(2018, 6, 8),
-                      'end': new Date(2018,6 , 11)
-                    }  ]}
+                  events={ this.state.data}
                     eventPropGetter={(this.eventStyleGetter)}
                 defaultDate={new Date()}/>
     );
